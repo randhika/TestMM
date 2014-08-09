125f3d766698d16f899631663bcb7dae4d41d51a
metromancn <metromancn@gmail.com>
2014-08-09 15:21:15 +0900

    DataManage

--- MM/app/src/main/java/com/xinlukou/metroman/engine/DataManage.java
+++ MM/app/src/main/java/com/xinlukou/metroman/engine/DataManage.java
@@ -3,8 +3,18 @@ package com.xinlukou.metroman.engine;
 import java.util.List;
 
 public class DataManage {
-    public List<SrcConfig> ConfigList = null;
-
+    public static List<SrcConfig> ConfigList = null;
+    public static List<String> HolidayList = null;
+    public static List<SrcUNO> UNOList = null;
+    public static List<SrcFare> FareList = null;
+    public static List<Station> StationList = null;
+    public static List<Line> LineList = null;
+    public static List<Way> WayList = null;
+    public static List<Link> LinkList = null;
+    public static List<Transfer> TransferFromList = null;
+    public static List<Transfer> TransferToList = null;
+    public static List<Timetable> WeekdayList = null;
+    public static List<Timetable> WeekendList = null;
 
     public static void InitData() {
         LoadConfig();
@@ -22,7 +32,19 @@ public class DataManage {
     }
 
     public static void ReleaseData() {
-
+        ConfigList = null;
+        HolidayList = null;
+        UNOList = null;
+        FareList = null;
+        StationList = null;
+        LineList = null;
+        WayList = null;
+        LinkList = null;
+        TransferFromList = null;
+        TransferToList = null;
+        WeekdayList = null;
+        WeekendList = null;
+        ConfigList = null;
     }
 
     private static void LoadConfig() {
