6fdd23e38553cce7fd144bd51cda58784567adb5
metromancn <metromancn@gmail.com>
2014-08-09 14:36:58 +0900

    MM
    Add MM from Android Studio

---
+++ MM/app/src/main/java/com/xinlukou/metroman/engine/DataManage.java
@@ -0,0 +1,75 @@
+package com.xinlukou.metroman.engine;
+
+import java.util.List;
+
+public class DataManage {
+    public List<SrcConfig> ConfigList = null;
+
+
+    public static void InitData() {
+        LoadConfig();
+        LoadHoliday();
+        LoadUNO();
+        LoadFare();
+        LoadStation();
+        LoadLine();
+        LoadWay();
+        LoadLink();
+        LoadTransferFrom();
+        LoadTransferTo();
+        LoadWeekday();
+        LoadWeekend();
+    }
+
+    public static void ReleaseData() {
+
+    }
+
+    private static void LoadConfig() {
+
+    }
+
+    private static void LoadHoliday() {
+
+    }
+
+    private static void LoadUNO() {
+
+    }
+
+    private static void LoadFare() {
+
+    }
+
+    private static void LoadStation() {
+
+    }
+
+    private static void LoadLine() {
+
+    }
+
+    private static void LoadWay() {
+
+    }
+
+    private static void LoadLink() {
+
+    }
+
+    private static void LoadTransferFrom() {
+
+    }
+
+    private static void LoadTransferTo() {
+
+    }
+
+    private static void LoadWeekday() {
+
+    }
+
+    private static void LoadWeekend() {
+
+    }
+}
---
+++ MM/app/src/main/java/com/xinlukou/metroman/engine/Link.java
@@ -0,0 +1,7 @@
+package com.xinlukou.metroman.engine;
+
+/**
+ * Created by QIUPING on 2014/8/9.
+ */
+public class Link {
+}
