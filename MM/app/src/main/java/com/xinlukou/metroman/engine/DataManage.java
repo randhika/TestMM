38a76b19c3065d9156801f29512c119035f3ddf7
metromancn <metromancn@gmail.com>
2014-08-10 13:55:42 +0900

    DM

--- MM/app/src/main/java/com/xinlukou/metroman/engine/DataManage.java
+++ MM/app/src/main/java/com/xinlukou/metroman/engine/DataManage.java
@@ -1,6 +1,8 @@
 package com.xinlukou.metroman.engine;
 
+import java.util.ArrayList;
 import java.util.List;
+import android.text.TextUtils;
 
 public class DataManage {
     public static List<SrcConfig> ConfigList = null;
@@ -47,51 +49,151 @@ public class DataManage {
         ConfigList = null;
     }
 
-    private static void LoadConfig() {
+    private static String GetPathByCity(String fileName) {
+        //TODO
+        return "";
+    }
 
+    private static String[] GetCsvFileContents(String fileName) {
+        String path = GetPathByCity(fileName);
+        //TODO
+        String content = "";
+        return content.split("\r\n");
+    }
+
+    private static void LoadConfig() {
+        if(ConfigList != null && ConfigList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("config");
+        ConfigList = new ArrayList<SrcConfig>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            ConfigList.add(new SrcConfig(str));
+        }
     }
 
     private static void LoadHoliday() {
-
+        if(HolidayList != null && HolidayList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("holiday");
+        HolidayList = new ArrayList<String>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            HolidayList.add(str);
+        }
     }
 
     private static void LoadUNO() {
-
+        if(UNOList != null && UNOList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("uno");
+        UNOList = new ArrayList<SrcUNO>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            UNOList.add(new SrcUNO(str));
+        }
     }
 
     private static void LoadFare() {
-
+        if(FareList != null && FareList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("fare");
+        FareList = new ArrayList<SrcFare>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            FareList.add(new SrcFare(str));
+        }
     }
 
     private static void LoadStation() {
-
+        if(StationList != null && StationList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("station");
+        Integer curIndex = 0;
+        StationList = new ArrayList<Station>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            StationList.add(new Station(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadLine() {
-
+        if(LineList != null && LineList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("line");
+        Integer curIndex = 0;
+        LineList = new ArrayList<Line>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            LineList.add(new Line(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadWay() {
-
+        if(WayList != null && WayList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("way");
+        Integer curIndex = 0;
+        WayList = new ArrayList<Way>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            WayList.add(new Way(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadLink() {
-
+        if(LinkList != null && LinkList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("link");
+        Integer curIndex = 0;
+        LinkList = new ArrayList<Link>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            LinkList.add(new Link(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadTransferFrom() {
-
+        if(TransferFromList != null && TransferFromList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("transferfrom");
+        Integer curIndex = 0;
+        TransferFromList = new ArrayList<Transfer>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            TransferFromList.add(new Transfer(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadTransferTo() {
-
+        if(TransferToList != null && TransferToList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("transferto");
+        Integer curIndex = 0;
+        TransferToList = new ArrayList<Transfer>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            TransferToList.add(new Transfer(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadWeekday() {
-
+        if(WeekdayList != null && WeekdayList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("weekday");
+        Integer curIndex = 0;
+        WeekdayList = new ArrayList<Timetable>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            WeekdayList.add(new Timetable(curIndex, str));
+            curIndex++;
+        }
     }
 
     private static void LoadWeekend() {
-
+        if(WeekendList != null && WeekendList.size() > 0) return;
+        String[] rowArray = GetCsvFileContents("weekend");
+        Integer curIndex = 0;
+        WeekendList = new ArrayList<Timetable>(rowArray.length);
+        for (String str : rowArray) {
+            if(TextUtils.isEmpty(str)) continue;
+            WeekendList.add(new Timetable(curIndex, str));
+            curIndex++;
+        }
     }
 }
--- MM/app/src/main/java/com/xinlukou/metroman/engine/SrcFare.java
+++ MM/app/src/main/java/com/xinlukou/metroman/engine/SrcFare.java
@@ -1,7 +1,31 @@
 package com.xinlukou.metroman.engine;
 
-/**
- * Created by QIUPING on 2014/8/9.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SrcFare {
+    public String FareType;
+    public String WayUNOList;
+    public String CommonFare;
+    public String FileFare;
+    public String StationUNOList;
+    public List<String> WayList;
+    public List<String> UNOList;
+    public String FileFareContent;
+    public List<String> FareList;
+
+    public SrcFare (String str) {
+        String[] array = str.split(",");
+        this.FareType = array[0];
+        this.WayUNOList = array[1];
+        this.CommonFare = array[2];
+        this.FileFare = array[3];
+        this.StationUNOList = array[4];
+        this.WayList = Arrays.asList(this.WayUNOList.split("|"));
+        this.UNOList = Arrays.asList(this.StationUNOList.split("|"));
+        if(!this.FileFare.isEmpty()) {
+            //TODO
+        }
+    }
 }
